0c3cdffa90bd7886b30d47afbeaa3c247d418c69
pysashapy <sasha.2000ibr@gmail.com>
2020-02-07 15:52:51 +0300

    Update parser_zen-ya.py 1

--- parser_zen-ya.py
+++ parser_zen-ya.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+#coding: UTF-8
 
 from bs4 import BeautifulSoup
 import requests as req
 import xlsxwriter
+from threading import Thread
 
 
-def pars_url(url, sheet,z):
+def pars_url(url, sheet,z,workbook):
     print(url)
     z2=z
     resp = req.get(url)
@@ -21,24 +23,35 @@ def pars_url(url, sheet,z):
         z2+=1
 
     url = soup.findAll('a', class_='pagination-prev-next__link')
+
     if url[-1]["href"] is not None and (len(url)==2 or url[0].text=="Следующие 20"):
-
+        if z%975==0:
+            workbook.close()
+            exit()
         url2="https://zen.yandex.ru"+url[-1]["href"]
+        try:
+            workbook2=workbook
+            pars_url(url2,sheet,z2,workbook2)
 
-        pars_url(url2,sheet,z2)
+        finally:
+            workbook.close()
     else: return "Сколько смог "
 
 def main():
-    url = "https://zen.yandex.ru/media/zen/channels?page=1"
-
-    workbook  = xlsxwriter.Workbook('filename.xlsx')
-    worksheet = workbook.add_worksheet()
-
+    url = "https://zen.yandex.ru/media/zen/channels?page=%s"
+    __LIST__ = []
     try:
-        pars_url(url,worksheet,1)
+        print("!")
+        a = int(input("Стр:"))
+        for i,z in zip(range(975*a+1,14000,975),range(1*a,20)):
+            print("2!")
+            workbook  = xlsxwriter.Workbook('filename%s.xlsx'%(z+2))
+            worksheet = workbook.add_worksheet()
+            pars_url(url%i,worksheet,1,workbook)
 
-    finally:
-        workbook.close()
+    except Exception as e:
+        pass
+
 if __name__ == '__main__':
 
     main()
